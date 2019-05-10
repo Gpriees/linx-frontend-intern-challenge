2d948ed668e6b8c0bd9b3fc012ba0e001e1a7157
Gpriees <g.priees@gmail.com>
2019-05-10 10:09:26 -0300

    import json

--- app/general.js
+++ app/general.js
@@ -5,5 +5,33 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
-    var product = request.response;
+    var products = request.response;
+    var productGrid = document.querySelector('.js-product-grid');
+    
+    if (!(products && productGrid)) {
+        return;
+    }
+
+    var productsHTML = products.reduce((html, product) => {
+        var instalment = '$' + (Number(product.price.replace('$', '')) / product.installmentTimes).toFixed(2);
+        return html += `
+            <li>
+                <article class="product">
+                <figure>
+                    <img src="${product.picture}" alt="${product.name}">
+                </figure>
+                <div class="content">
+                    <p class="name">${product.name}</p>
+                    <p class="description">${product.description}</p>
+                    <p class="old-price">De: R${product.oldPrice}</p>
+                    <p class="price">Por: R${product.price}</p>
+                    <p class="instalment">ou ${product.installmentTimes}x de ${instalment}</p>
+                    <button class="buy" onclick="alert('Produto não adicionado ao seu carinho!')">Comprar</button>
+                </div>
+                </article>
+            </li>
+        `;
+    }, '');
+
+    productGrid.innerHTML = productsHTML;
 }
